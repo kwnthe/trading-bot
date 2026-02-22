b2aa09c1b025c93c1419505b2220afc0c515df52
konstantinos-th <konstantinos@photoboothsupplyco.com>
2026-02-22 20:08:29 +0200

    Remove web-app folder

--- web-app/trading_dashboard/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from backtests.consumers import LiveChartConsumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/live/(?P<session_id>[\w-]+)/$', LiveChartConsumer.as_asgi()),
-]
